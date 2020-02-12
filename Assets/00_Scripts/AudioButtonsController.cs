446f2eb5fc18f530ede72b2afbe3fa391c629466
Patrick Rainer <patrick.rainer2@gmail.com>
2020-02-12 06:12:49 +0100

    042 - Mute and Unmute functionality

---
+++ Assets/00_Scripts/AudioButtonsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using UnityEngine.UI;
+using System;
+using UnityEngine.Events;
+
+public class AudioButtonsController : MonoBehaviour
+{
+    private Button myButton;
+
+    private void Awake()
+    {
+        myButton = GetComponent<Button>();
+    }
+
+    public void Mute()
+    {
+        AudioListener.pause = true;
+    }
+
+    public void UnMute()
+    {
+        AudioListener.pause = false;
+    }
+
+    private void OnDisable()
+    {
+        myButton.onClick.RemoveAllListeners();
+    }
+
+
+}
---
+++ Assets/00_Scripts/SoundManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public static void MuteAllSound()
+    {
+        AudioListener.pause = true;
+    }
+
+    public static void UnMuteAllSound()
+    {
+        AudioListener.pause = false;
+    }
+}
