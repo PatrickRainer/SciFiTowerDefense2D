24915fd9361e0fdeb6e81576e7f2614c40a06688
Patrick Rainer <patrick.rainer2@gmail.com>
2020-02-21 05:29:09 +0100

    060 - LevelMenu and Save Load Mechanic for Levels

---
+++ Assets/00_Scripts/LevelButtonController.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using Sirenix.OdinInspector;
+using UnityEngine.UI;
+using Doozy.Engine.UI;
+using System;
+
+public class LevelButtonController : MonoBehaviour
+{
+    [TitleGroup("References")]
+    [SerializeField, Required]
+    private Image star1;
+    [SerializeField, Required]
+    private Image star2;
+    [SerializeField, Required]
+    private Image star3;
+
+    [TitleGroup("Settings")]
+    [SerializeField, Required, Tooltip("Use the BuildName")]
+    private string levelName;
+    [SerializeField]
+    private Color starNotActiveColor;
+    [SerializeField]
+    private Color starActiveColor;
+
+    private Button myButton;
+    private UIButton myDoozyButton;
+
+    private void Awake()
+    {
+        myButton = GetComponentInParent<Button>();
+        myDoozyButton = GetComponentInParent<UIButton>();
+        star1.color = starNotActiveColor;
+        star2.color = starNotActiveColor;
+        star3.color = starNotActiveColor;
+    }
+
+    private void Start()
+    {
+        SetButtonActivity();
+        SetStars();
+    }
+       
+    private void SetButtonActivity()
+    {
+        if (LevelSaveLoadController.IsLevelLocked(levelName))
+        {
+            myDoozyButton.Interactable = false;
+        }
+        else
+        {
+            myDoozyButton.Interactable = true;
+        }
+    }
+       
+    private void SetStars()
+    {
+        int _stars = LevelSaveLoadController.GetStarsAchieved(levelName);
+
+        switch (_stars)
+        {   case 1:
+                star1.color = starActiveColor;
+                break;
+            case 2:
+                star1.color = starActiveColor;
+                star2.color = starActiveColor;
+                break;
+            case 3:
+                star1.color = starActiveColor;
+                star2.color = starActiveColor;
+                star3.color = starActiveColor;
+                break;
+            default:
+                break;
+        }
+
+    }
+
+
+}
---
+++ Assets/00_Scripts/LevelSaveLoadController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class LevelSaveLoadController : MonoBehaviour
+{
+
+    public static bool IsLevelLocked(string levelName)
+    {
+        int _isLevelLocked = PlayerPrefs.GetInt(levelName + "IsLocked", 1);
+
+        if (_isLevelLocked == 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static int GetStarsAchieved(string levelName)
+    {
+        return PlayerPrefs.GetInt(levelName + "Stars", 0);
+    }
+
+    public static void SetLockLevel(string levelName, bool isLocked)
+    {
+        int _isLocked;
+        if (isLocked)
+        {
+            _isLocked = 1;
+        }
+        else
+        {
+            _isLocked = 0;
+        }
+
+        PlayerPrefs.SetInt(levelName + "IsLocked", _isLocked);
+        PlayerPrefs.Save();
+    }
+
+    public static void SetStars(string levelName, int stars)
+    {
+        PlayerPrefs.SetInt(levelName + "Stars", stars);
+        PlayerPrefs.Save();
+    }
+
+    // Testing
+    [TitleGroup("Testing")]
+    [SerializeField]
+    private string levelName;
+    [SerializeField]
+    private bool doLock = false;
+    [SerializeField]
+    private int stars = 0;
+    [ResponsiveButtonGroup]
+    private void SetLockLevel()
+    {
+        SetLockLevel(levelName, doLock);
+    }
+    [ResponsiveButtonGroup]
+    private void SetStars()
+    {
+        SetStars(levelName, stars);
+    }
+    [ResponsiveButtonGroup]
+    private void ClearPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
