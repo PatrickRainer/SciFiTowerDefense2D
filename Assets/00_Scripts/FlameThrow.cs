39b568a484ad8a236e5dbe77c9cf70a885b0bd19
Patrick Rainer <patrick.rainer2@gmail.com>
2020-02-29 07:29:34 +0100

    084 - FlameThrower Sound

--- Assets/00_Scripts/FlameThrow.cs
+++ Assets/00_Scripts/FlameThrow.cs
@@ -2,11 +2,21 @@
 using System.Collections;
 using Sirenix.OdinInspector;
 
-[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Collider2D)), RequireComponent(typeof(AudioSource))]
 public class FlameThrow : MonoBehaviour
 {
     [SerializeField]
     private int damage = 1;
+
+    private void OnEnable()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+
+    private void OnDisable()
+    {
+        GetComponent<AudioSource>().Stop();
+    }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
