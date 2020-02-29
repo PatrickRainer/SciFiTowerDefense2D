364fa03313659ba68f44867281498989b0a2499d
Patrick Rainer <patrick.rainer2@gmail.com>
2020-02-29 11:54:18 +0100

    085 - Nova Bullet Movement, and sound

--- Assets/00_Scripts/Bullet.cs
+++ Assets/00_Scripts/Bullet.cs
@@ -32,45 +32,48 @@ public class Bullet : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField]
-    private float speed = 10;
+    protected float speed = 10;
     [SerializeField]
-    private int damage;
+    protected int damage;
     [SerializeField]
-    private AudioClip bulletSound;
+    protected AudioClip bulletSound;
 
     [HorizontalGroup("Base")]
     [VerticalGroup("Base/Left")]
     [SerializeField, ReadOnly, PreviewField(ObjectFieldAlignment.Left), HideLabel]
-    private GameObject target;
+    protected GameObject target;
     [VerticalGroup("Base/Right")]
     [SerializeField, ReadOnly]
-    private Vector3 startPosition;
+    protected Vector3 startPosition;
     [SerializeField, ReadOnly]
     [VerticalGroup("Base/Right")]
-    private Vector3 targetPosition;
+    protected Vector3 targetPosition;
     [SerializeField, ReadOnly]
     [VerticalGroup("Base/Right")]
-    private Vector3 worldPosition;
+    protected Vector3 worldPosition;
     [ReadOnly]
     [VerticalGroup("Base/Right")]
     public Tower myTower;
 
-    private float distance;
-    private float startTime;
+    protected float distance;
+    protected float startTime;
 
     public GameObject Target { get => target; set => target = value; }
     public Vector3 StartPosition { get => startPosition; set => startPosition = value; }
     public Vector3 TargetPosition { get => targetPosition; set => targetPosition = value; }
     public int Damage { get => damage; set => damage = value; }
 
-    private void Start()
+    protected virtual void Start()
     {
         startTime = Time.time;
         distance = Vector2.Distance(TargetPosition, StartPosition);
-        AudioSource.PlayClipAtPoint(bulletSound, gameObject.GetPosition());
+        if (bulletSound != null)
+        {
+            AudioSource.PlayClipAtPoint(bulletSound, gameObject.GetPosition());
+        }      
     }
 
-    private void Update()
+    protected virtual void Update()
     {
         worldPosition = transform.position;
 
@@ -84,7 +87,7 @@ public class Bullet : MonoBehaviour
         } 
     }
 
-    private bool IsTargetPositionReached()
+    protected virtual bool IsTargetPositionReached()
     {
         // Fixed Bug: Need to round the vectors, because else the enemy is sometimes just a pont later
         // and the compare of the two vectors does not work then
