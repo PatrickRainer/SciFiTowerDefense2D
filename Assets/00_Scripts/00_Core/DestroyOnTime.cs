c5d2a9b64c3724c2943c413954cd8ceae47821e4
Patrick Rainer <patrick.rainer2@gmail.com>
2020-03-05 14:39:28 +0100

    090 - Level Design, Destroy on Time for Bullets an Effects, Pivot Parent for a Platform

---
+++ Assets/00_Scripts/00_Core/DestroyOnTime.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnTime : MonoBehaviour
+{
+    [SerializeField]
+    private float lifeTime = 10f;
+
+    private void Start()
+    {
+        Destroy(gameObject, 10f);
+    }
+}
